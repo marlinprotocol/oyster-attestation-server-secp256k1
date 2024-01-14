8b03ef0c564f90d1ce699792bb316cac8c6e8017
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-01-14 13:27:52 +0530

    use local AppState

--- src/handler.rs
+++ src/handler.rs
@@ -1,9 +1,14 @@
-use crate::types::AppState;
-
 use actix_web::{error, http::StatusCode, post, web, Responder};
 use libsodium_sys::crypto_sign_detached;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+
+pub struct AppState {
+    pub ed25519_secret: [u8; 32],
+    pub secp256k1_public: [u8; 65],
+    pub attestation_uri: String,
+    pub max_age: usize,
+}
 
 #[derive(Deserialize, Serialize)]
 struct AttestationVerificationBuilderResponse {
